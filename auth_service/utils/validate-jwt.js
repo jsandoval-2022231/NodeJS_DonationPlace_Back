8f1e34a9e88122f6934519b1865e79b30e0c365d
David Sandoval <jsandoval-2022231@kinal.edu.gt>
2024-07-17 23:24:05 -0600

    bart'on god

--- auth_service/utils/validate-jwt.js
+++ auth_service/utils/validate-jwt.js
@@ -1,19 +1,21 @@
 import jwt from 'jsonwebtoken';
 
 export const validateJWT = (req, res, next) => {
-    let token = req.headers['authorization']
+    let token = req.body.token || req.query.token || req.headers['authorization']
 
     if (!token) {
-        return res.status(401).send('A token is required for authentication');
+        return res.status(401).send('A token is required for authentication')
     }
 
     try {
-        token = token.replace(/^Bearer\s+/, '');
-        const decoded = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-        req.user = decoded.userId;
+        token = token.replace(/^Bearer\s+/, '')
+        const decoded = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
+
+        req.user = decoded
     } catch (e) {
-        return res.status(401).send('Invalid Token');
+        console.log(e)
+        return res.status(401).send('Invalid Token')
     }
 
-    return next();
-};
+    return next()
+}
